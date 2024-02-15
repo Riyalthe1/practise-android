b5e2aa6d7794d3310f18f86e1522098e48da3e64
Riyal Riyan <riyel1422@gmail.com>
2024-02-15 22:00:56 +0600

    added youtube and facebook profile info

--- app/src/main/java/com/example/practise/MainActivity.kt
+++ app/src/main/java/com/example/practise/MainActivity.kt
@@ -5,7 +5,9 @@ import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -16,14 +18,17 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.practise.ui.theme.PractiseTheme
 
 class MainActivity : ComponentActivity() {
@@ -38,7 +43,7 @@ class MainActivity : ComponentActivity() {
 }
 
 
-val fontFamily= FontFamily(
+val fontFamily = FontFamily(
     Font(
         R.font.f, weight = FontWeight.Bold
     )
@@ -53,13 +58,49 @@ fun Portfolio() {
         modifier = Modifier
             .padding(12.dp)
             .fillMaxWidth()
-    ){ Image(painter = painterResource(id = R.drawable.contact), contentDescription = null, modifier = Modifier.size(60.dp))
-        Divider()
-        Column {
-Text(text = "hello riyalthe1", style = TextStyle(color = Color.Unspecified, fontFamily = fontFamily, fontWeight = FontWeight.Bold
-
-))
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Image(
+                painter = painterResource(id = R.drawable.contact),
+                contentDescription = null,
+                modifier = Modifier.size(60.dp)
+            )
+            Divider()
+            Text(
+                text = "hello riyalthe1",
+                style = TextStyle(
+                    color = Color.Magenta,
+                    fontFamily = fontFamily,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = MaterialTheme.typography.headlineLarge.fontSize
+                )
+            )
+            Text(
+                text = "android devoloper beginner",
+                style = TextStyle(
+                    fontFamily = fontFamily,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = MaterialTheme.typography.bodyLarge.fontSize
+                ),
+                modifier = Modifier.padding(bottom = 10.dp)
+            )
+            Row {
+                Image(
+                    painter = painterResource(id = R.drawable.fb),
+                    contentDescription = "facebook",
+                    modifier = Modifier.size(20.dp)
+                )
+                Text(text = "/fafaoif", modifier = Modifier.padding(start = 10.dp))
+            }
+            Row {
+                Image(
+                    painter = painterResource(id = R.drawable.yt),
+                    contentDescription = "Youtube",
+                    modifier = Modifier.size(20.dp)
+                )
+                Text(text = "/fafaoif", modifier = Modifier.padding(start = 10.dp))
+            }
         }
     }
 
-    }
+}
